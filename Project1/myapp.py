8854e8c61b605e63ac6e46cd39df8ab46197a92d
Cl4ryty <61427823+Cl4ryty@users.noreply.github.com>
2023-11-03 22:12:35 +0100

    Simple crawler and search engine app,  completing week 1 and week 2 tasks as well as some improvements (week 3 tasks)

---
+++ Project1/myapp.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, render_template
+from whoosh import index
+ix = index.open_dir("index")
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def start():
+    return render_template("start.html", title='start')
+
+@app.route("/search")
+def search():
+    if not 'q' in request.args:
+        return "No search term provided, go back to the <a href=\"/\">home page</a>"
+    else:
+        search_term = request.args['q']
+        result_list = []
+        # Retrieving data
+        from whoosh.qparser import QueryParser
+        with ix.searcher() as searcher:
+            # find entries with the words 'first' AND 'last'
+            query = QueryParser("content", ix.schema).parse(search_term)
+            results = searcher.search(query)
+
+            # get the important things from the results to use in the template
+            for r in results:
+                result_list.append((r["url"], r["title"], r.highlights("content")))
+
+        return render_template("search.html", title=search_term, search_term=search_term, result=result_list)
+    
