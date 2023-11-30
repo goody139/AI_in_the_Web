4146ed3a50fef87054bfe173f446664927b5ea87
Cl4ryty <61427823+Cl4ryty@users.noreply.github.com>
2023-11-30 22:51:33 +0100

    include sounds and cleanup

--- Project1/unicorn_search.py
+++ Project1/unicorn_search.py
@@ -51,8 +51,41 @@ def search():
             for r in ranked_results:
                 result_list.append((r["url"], r["title"], r["highlights"]))
 
-        return render_template("search2.html", title=search_term, search_term=search_term, result=result_list, suggestion=suggestion)
+        return render_template("search.html", title=search_term, search_term=search_term, result=result_list, suggestion=suggestion)
     
 @app.errorhandler(500)
+@app.errorhandler(501)
+@app.errorhandler(502)
+@app.errorhandler(503)
+@app.errorhandler(504)
+@app.errorhandler(505)
 def internal_error(exception):
    return render_template("server_error.html", title='Server error')
+
+
+@app.errorhandler(400)
+@app.errorhandler(401)
+@app.errorhandler(403)
+@app.errorhandler(404)
+@app.errorhandler(405)
+@app.errorhandler(406)
+@app.errorhandler(408)
+@app.errorhandler(409)
+@app.errorhandler(411)
+@app.errorhandler(412)
+@app.errorhandler(413)
+@app.errorhandler(414)
+@app.errorhandler(415)
+@app.errorhandler(416)
+@app.errorhandler(417)
+@app.errorhandler(418)
+@app.errorhandler(418)
+@app.errorhandler(422)
+@app.errorhandler(423)
+@app.errorhandler(424)
+@app.errorhandler(428)
+@app.errorhandler(429)
+@app.errorhandler(431)
+@app.errorhandler(451)
+def client_error(exception):
+   return render_template("client_error.html", title='Client error')
