588bf8c280fc67ae0baff564688b799b6ccac1a5
goody139 <lgolla@uni-osnabrueck.de>
2023-12-12 15:41:04 +0100

    First version recommmender
    Using pearson similarity a recommender system based on similar items is implemented

--- Project2/helper_functions.py
+++ Project2/helper_functions.py
@@ -1,6 +1,7 @@
 from imdb import IMDb
 from models import Movie, MovieGenre, Rating, Tag, Link, Image
 import requests 
+from scipy.stats import pearsonr
 
 """ An api key is needed """
 
@@ -36,7 +37,7 @@ def create_content(movies):
     # Versuche die Datei zu öffnen und ihren Inhalt zu lesen
     with open(dateipfad, 'r') as datei:
         inhalt = datei.read()
-        print(inhalt)
+        #print(inhalt)
 
     content = []
     for movie in movies: 
@@ -44,7 +45,6 @@ def create_content(movies):
         #print(links.tmdbid)
         url = "https://api.themoviedb.org/3/movie/{0}?api_key={1}".format(links.tmdbid,inhalt)
         data = requests.get(url)
-        print(data)
         data = data.json()
         #print(data)
         if 'overview' not in data:
@@ -78,6 +78,8 @@ def movie_poster(movies):
     link_list = []
     for movie in movies: 
         m = Image.query.filter_by(movieid=movie.id).first()
+        if m == None: 
+            return None
         link_list.append(m.link)
     
     return link_list
@@ -92,10 +94,77 @@ def create_tags(movies):
             tag_list.append(t.tag)
         tag_list = set(tag_list)
         final_list.append(tag_list)
+    print("final tag list")
     print(final_list)
         
     
     return final_list
 
+def get_movies(movie_ids):
+    movies = []
+    for id in movie_ids: 
+        movie = Movie.query.get(id)
+        movies.append(movie)
+    return movies
 
-        
+def get_user_ratings(user_id):
+    user_ratings = Rating.query.filter_by(user_id=user_id).all()
+    return {rating.movie_id: rating.rating_value for rating in user_ratings}
+
+
+def pearson_similarity(user1_ratings, user2_ratings):
+    common_movies = set(user1_ratings.keys()) & set(user2_ratings.keys())
+
+    if not common_movies:
+        return 0  # No common movies, similarity is 0
+
+    user1_values = [user1_ratings[movie] for movie in common_movies]
+    user2_values = [user2_ratings[movie] for movie in common_movies]
+
+    correlation_coefficient, _ = pearsonr(user1_values, user2_values)
+    return correlation_coefficient
+
+
+
+# sowas wie genre als option? 
+# wenn knopf gedrückt wird für bestimmtes genre?    
+def get_recommendations(userId, genre):
+    user_ratings = get_user_ratings(userId)
+    all_users = Rating.query.filter(Rating.user_id != userId).limit(100)
+
+    print("calculating similarities")
+    similarities = []
+    for other_user in all_users:
+        other_user_ratings = get_user_ratings(other_user.user_id)
+        similarity = pearson_similarity(user_ratings, other_user_ratings)
+        similarities.append((other_user.user_id, similarity))
+
+    similarities.sort(key=lambda x: x[1], reverse=True)
+
+    print("selecting recommended movies")
+    recommended_movies = set()
+    for other_user_id, similarity in similarities:
+        other_user_ratings = get_user_ratings(other_user_id)
+        for movie_id, rating in other_user_ratings.items():
+            if movie_id not in user_ratings and genre != None:
+                movie = MovieGenre.query.filter(MovieGenre.movie_id == movie_id).all()
+                g = False
+                for m in movie: 
+                    if m.genre == genre: 
+                        g = True
+                if movie and g == True:
+                    recommended_movies.add((movie_id, rating * similarity))
+
+            elif movie_id not in user_ratings and genre == None: 
+                print("should not be entered")
+                recommended_movies.add((movie_id, rating * similarity))
+
+    recommended_movies = list(recommended_movies)
+    recommended_movies.sort(key=lambda x: x[1], reverse=True)
+
+    movie_id_list = []
+    for movie in recommended_movies: 
+        if movie[0] not in movie_id_list:
+            movie_id_list.append(movie[0])
+
+    return movie_id_list[:10]
--- Project2/recommender.py
+++ Project2/recommender.py
@@ -5,8 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
 from flask_user import login_required, UserManager, UserMixin, current_user
 from read_data import check_and_read_data
-from helper_functions import create_links, get_movie_poster_links, movie_poster, create_tags, create_content
-from models import User, Movie, MovieGenre, Rating, Link, Tag, db
+from helper_functions import *
+from models import *
 
 
 # Running on http://127.0.0.1:5000/
@@ -18,10 +18,9 @@ from models import User, Movie, MovieGenre, Rating, Link, Tag, db
 movie IDS :
 keys = [128, 678, 791, 875, 979, 1107, 1423, 2851, 4051, 4207, 4568, 5069, 5209, 7646, 7669, 7762, 7841, 7842, 26453, 26587, 26614, 26649, 26693, 26761, 26849, 26887, 27002, 27036, 27251, 27611, 27708, 27751, 31193, 32294, 32600, 38198, 40697, 42602, 49917, 51024, 52281, 53883, 55207, 57772, 61406, 62336, 62970, 63433, 64167, 65135, 65359, 66544, 66934, 69524, 69849, 70521, 71160, 72982, 77177, 79299, 84847, 85780, 86237, 86487, 90647, 90945, 92475, 93008, 93040, 93988, 95717, 95738, 96471, 96518, 96520, 99532, 99764, 100044, 100553, 105250, 106642, 107780, 108727, 115111, 115969, 121035, 122260, 122926, 126430, 127180, 127390, 131724, 137859, 139130, 140481, 140737, 141816, 142115, 147250, 147328, 147330, 148675, 150548, 151763, 152284, 152711, 159817, 163809, 167570, 170705, 171011, 171495, 171749, 172497, 172825, 172909, 173535, 173873, 174053, 174403, 175693, 176329, 178129, 179135, 180263, 180777, 184257, 185135, 193579]
 Einfach so lassen? Oder mit der Google API nocheinmal versuchen zu füllen? 
+
 - wie timestaps händeln? : "wenn advanced recommender system benötigt" Thelen
-- movielens anmeldung ?
 - wenn rating gerated worden ist farbe ändern oder so? Oder sterne system
-- Content auch von API accessen? und in Database speichern?
 - Rating algorithm : 
     For a given user: 
         find similar values 
@@ -60,8 +59,6 @@ db.init_app(app)  # initialize database
 db.create_all()  # create database if necessary
 user_manager = UserManager(app, db, User)  # initialize Flask-User management
 
-
-
 # Create all database tables if not initialized yet 
 check_and_read_data(db)
 
@@ -79,28 +76,12 @@ def movies_page():
     # IDEE : zum anklicken machen - based on tags - (Your Genre! / Your artists! / Your Vibe!)
     # so etwas zum sliden? 
 
-    # bilder in html displayen für movie ID 
-
     user= current_user
-    print(user.username)
-    print(user.id)
+    user_id = user.id
 
-    # all movies rated with 5.0 stars 
-    ratings = Rating.query.filter(Rating.rating_value == 5)#.limit(10).all()
-    Movie_ids = []
-    for r in ratings:
-        Movie_ids.append(r.movie_id)
-
-
-    # Idee für genre button einrichten oder so? 
-
-    # provides output for one user with a specific ranking and a specific genre
-    movies = db.session.query(Movie).join(Rating).join(MovieGenre).filter(
-        Rating.user_id == user.id,
-        Rating.rating_value >= 1,
-        MovieGenre.genre == "Comedy", 
-    ).limit(10).all()
-    print(movies)
+    genre = "Horror"
+    movies = get_recommendations(user_id, genre)
+    movies = get_movies(movies)
 
     # result list 
     MOVIEID, IMDB, TMBID = create_links(movies) 
